6de3e95f02bf3c1bc578839acd6a2fabc8d4e4c7
Lalu Oldi Alfitra <oldia9@gmail.com>
2019-04-24 21:11:52 +0700

    Lalu Oldi Alfitra

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const server = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://hacktivoverflow-server.oldialfitra.com',
 });
 
 export default server;
